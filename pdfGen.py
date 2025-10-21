22e07bab91e37c8e8f0ea2a5c22a68b03f815c8f
ravi <sdkravi1@gmail.com>
2025-10-21 15:23:35 +0530

    add footer

--- pdfGen.py
+++ pdfGen.py
@@ -2,6 +2,8 @@ from fpdf import FPDF as fpdf
 import pandas as pd
 
 pdf = fpdf(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
+
 df = pd.read_csv('topics.csv')
 
 for index, row in df.iterrows():
@@ -12,9 +14,23 @@ for index, row in df.iterrows():
     pdf.set_text_color(255, 0, 0)
     pdf.cell(w=0, h=14, txt=topic, ln=1, align='L')
     pdf.line(10, 20, 200, 20)
+
+    # Footer
+    pdf.ln(265)
+    pdf.set_font("Arial", size=10, style='B')
+    pdf.set_text_color(200, 200, 200)
+    pdf.cell(w=0, h=10, txt=topic, ln=1, align='R')
+
+
     for i in range(pages-1):
         pdf.add_page()
+
+        # Footer
+        pdf.ln(279)
+        pdf.set_font("Arial", size=10, style='B')
+        pdf.set_text_color(200, 200, 200)
+        pdf.cell(w=0, h=10, txt=topic, ln=1, align='R')
         
 
-pdf.output("topics.pdf")
+pdf.output("Topics.pdf")
 
