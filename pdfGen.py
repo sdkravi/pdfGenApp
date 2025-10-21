206d4ccd5a1699e5aea41cdf6d9c18a39ca22f71
ravi <sdkravi1@gmail.com>
2025-10-21 14:55:27 +0530

    add multiple blank pages of each topic

--- pdfGen.py
+++ pdfGen.py
@@ -6,11 +6,15 @@ df = pd.read_csv('topics.csv')
 
 for index, row in df.iterrows():
     pdf.add_page()
-    row = row["Topic"]
+    topic = row["Topic"]
+    pages = row['Pages']
     pdf.set_font("Times", size=14, style='B')
     pdf.set_text_color(255, 0, 0)
-    pdf.cell(w=0, h=14, txt=row, ln=1, align='L')
+    pdf.cell(w=0, h=14, txt=topic, ln=1, align='L')
     pdf.line(10, 20, 200, 20)
+    for i in range(pages-1):
+        pdf.add_page()
+        
 
 pdf.output("topics.pdf")
 
